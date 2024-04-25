b40a1909f2e615ae1afdf14d5b17652f2c8a8d8c
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2024-04-25 13:06:04 -0300

    fetching api, cards styled

--- screens/overview.tsx
+++ screens/overview.tsx
@@ -1,26 +1,49 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { ScreenContent } from 'components/ScreenContent';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 
 import { Button } from '../components/Button';
 import { RootStackParamList } from '../navigation';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
 
 type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Overview'>;
 
 export default function Overview() {
   const navigation = useNavigation<OverviewScreenNavigationProps>();
+  const [animeList, setAnimeList] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchAnime = async ()=>{
+
+      const response = await fetch('https://api.jikan.moe/v4/top/anime');
+      const data = await response.json();
+      setAnimeList(data.data);
+    }
+
+    fetchAnime();
+  }, [])
+
+  const renderAnimeItem = ({ item }) => (
+    <TouchableOpacity>
+      <View style={styles.card}>
+        <Image
+        style={styles.animeImage}
+        source={{ uri: item.images.jpg.large_image_url }}  
+        />
+        <Text style={styles.animeTitle}>{item.title}</Text>
+        
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <ScreenContent path="screens/overview.tsx" title="Overview" />
-      <Button
-        onPress={() =>
-          navigation.navigate('Details', {
-            name: 'Dan',
-          })
-        }
-        title="Show Details"
+      <Text style={styles.title}>Anime List</Text>
+      <FlatList
+        data={animeList}
+        renderItem={renderAnimeItem}
       />
     </View>
   );
@@ -30,5 +53,36 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+
   },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    textAlign: 'center',
+    marginBottom: 30
+  },
+
+  animeTitle: {
+    fontSize: 20,
+    padding: 10,
+    marginBottom: 5,
+    fontWeight: 'bold'
+  },
+
+  animeImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10 
+  },
+
+  card: {
+    flex: 1,
+    padding: 15,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: '#ffff',
+    elevation: 3,
+    flexDirection: 'row'
+  }
 });
